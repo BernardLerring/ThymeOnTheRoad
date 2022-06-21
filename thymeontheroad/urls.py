733303074ca40c660b6499df8af0cb6d1fd7ad3c
Tim <orcatim@hotmail.com>
2022-06-21 07:39:18 +0000

    style login form

--- thymeontheroad/urls.py
+++ thymeontheroad/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('tinymce/', include('tinymce.urls')),
-    path("", include('recipesite.urls'), name='recipesite_urls')
+    path("", include('recipesite.urls'), name='recipesite_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
